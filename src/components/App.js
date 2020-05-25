8903c42faa34f5a431ab10a4693ac78b349a5b01
rahuljha661 <rahuljha12527@gmail.com>
2020-05-25 22:54:16 +0530

    subscribe to the code

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import {data} from '../data';
+ import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 
 
-function App() {
+
+
+
+class App extends React.Component {
+  
+   componentDidMount(){
+    const {store}=this.props;
+    
+    store.subscribe(()=>{
+      console.log('Updated');
+      this.forceUpdate();
+    });
+    
+    //  make api call
+    // dispatch  action
+    store.dispatch({
+      type:'ADD_MOVIES',
+      movies: data
+    });
+
+    console.log('STATE',this.props.store.getState());
+
+   }
+  render(){
+  const movies=this.props.store.getState();
+   console.log('RENDER');
+
   return (
     <div className="App">
          <Navbar />
@@ -17,14 +43,15 @@ function App() {
 
            </div>
            <div className="list">
-            {data.map(movie =>(
-              <MovieCard movie={movie}/>
+            {movies.map((movie ,index)=>(
+              <MovieCard movie={movie} key={`movies-${index}`}/>
             ))}
            </div>
 
          </div>
          </div>
   );
+}
 }
 
 export default App;
